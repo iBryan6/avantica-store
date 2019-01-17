86a17bb9959246aab402fec96b96ce32f39d41a5
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-17 19:41:07 -0400

    navbar finished

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,4 +1,17 @@
-        <nav class="navbar navbar-expand-md bg-primary navbar-dark sticky-top">
+        <nav class="navbar navbar-expand-md bg-light navbar-light">
+               <ul class="navbar-nav navbar-brand ml-auto">
+                    <li class="nav-item navbar-brand"> <a class="nav-link">Contact Us</a></li> 
+                    @if (Route::has('register'))
+                    <li class="nav-item navbar-brand">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            </ul>
+            <ul class ="navbar-nav ml-auto">
+                    
+            </ul>
+        </nav>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
             
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="images/icnorte.png" alt="IC NORTE Logo">
@@ -6,12 +19,9 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
+                <div class="collapse navbar-collapse ml-right" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
                     <ul class="navbar-nav">
                             <li class="nav-item dropdown">
                               <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target" href="#">Shopping
@@ -24,25 +34,57 @@
                                   <a href="#" class="dropdown-item">Cloths</a>
                               </div>
                             </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="/about">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="/">Link</a>
-                            </li> 
+                            <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target" href="#">Shopping
+                                        <span class="caret"></span>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdown_target">
+                                        <a href="#" class="dropdown-item">Electronics</a>
+                                        <a href="#" class="dropdown-item">Food</a>
+                                        <a href="#" class="dropdown-item">Drinks</a>
+                                        <a href="#" class="dropdown-item">Cloths</a>
+                                    </div>
+                                  </li>
+                                  <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target" href="#">Shopping
+                                            <span class="caret"></span>
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdown_target">
+                                            <a href="#" class="dropdown-item">Electronics</a>
+                                            <a href="#" class="dropdown-item">Food</a>
+                                            <a href="#" class="dropdown-item">Drinks</a>
+                                            <a href="#" class="dropdown-item">Cloths</a>
+                                        </div>
+                                      </li>
+                                      <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown_target" href="#">Shopping
+                                                <span class="caret"></span>
+                                            </a>
+                                            <div class="dropdown-menu" aria-labelledby="dropdown_target">
+                                                <a href="#" class="dropdown-item">Electronics</a>
+                                                <a href="#" class="dropdown-item">Food</a>
+                                                <a href="#" class="dropdown-item">Drinks</a>
+                                                <a href="#" class="dropdown-item">Cloths</a>
+                                            </div>
+                                          </li>
                           </ul>
+                        </div>
+                          <ul class="navbar-nav ml-auto mr-auto">
+                          <form class="form-inline">
+                                <input class="form-control mr-sm-2 input-lg" type="text" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                          </form>
+                        </ul>
+                 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                            <ol><button class="btn btn-secondary"><i class="fas fa-shopping-cart fa-2x"></i>Cart Items</button>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                           
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
