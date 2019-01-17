f7839d148c17815f181fb323fe05254f1887d6ba
Bryan Argandoña <bryan.argandona@avantica.net>
2019-01-17 18:44:22 -0400

    laravel mix

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,13 +2,21 @@
     <div class="row">
       <div class="col-12 col-md">
         <h6><a href="tel:+59176953543"><i class="fas fa-phone"></i> +591 76953543</a></h6>
-        <h6><a href="mailto:avantica_store@gmail.com"><i class="fas fa-envelope"></i> avantica_store@gmail.com</a></h6><hr>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Features</h5>
+        <h6><a href="mailto:avantica_store@gmail.com"><i class="fas fa-envelope"></i> avantica_store@gmail.com</a></h6>
+        <hr>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Cool stuff</a></li>
-          <li><a class="text-muted" href="#">Random feature</a></li>
+            <li><a class="text-muted" href="#">Cool stuff</a></li>
+            <li><a class="text-muted" href="#">Random feature</a></li>
+            <li><a class="text-muted" href="#">Team feature</a></li>
+        </ul>        
+      </div>
+      <div class="col-6 col-md icons">
+        <a href=""><i class="fab fa-facebook-square fa-3x"></i></a>
+        <a href=""><i class="fab fa-facebook-square fa-3x"></i></a>
+        <a href=""><i class="fab fa-facebook-square fa-3x"></i></a>
+        <a href=""><i class="fab fa-facebook-square fa-3x"></i></a>
+        <hr>
+        <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Team feature</a></li>
           <li><a class="text-muted" href="#">Stuff for developers</a></li>
           <li><a class="text-muted" href="#">Another one</a></li>
