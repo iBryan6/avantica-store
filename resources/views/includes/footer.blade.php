0a4ce0fcb1d1fc232289b71422dd5f4d65661aec
Bryan Argandoña <bryan.argandona@avantica.net>
2019-01-18 10:01:22 -0400

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top footer container-fluid">
     <div class="row">
-      <div class="col-12 col-md">
+      <div class="col-6 col-md">
         <h6><a href="tel:+59176953543" class="footer-phone"><i class="fas fa-phone"></i> +591 76953543</a></h6><br>
-        <h6><a href="mailto:avantica_store@gmail.com"><i class="fas fa-envelope"></i> avantica_store@gmail.com</a></h6><br>
+        <h5><a href="mailto:avantica_store@gmail.com"><i class="fas fa-envelope"></i> avantica_store@gmail.com</a></h5><br>
         <ul class="list-unstyled text-small">
-            <li><a class="text-muted" href="#">Cool stuff</a></li>
-            <li><a class="text-muted" href="#">Random feature</a></li>
-            <li><a class="text-muted" href="#">Team feature</a></li>
+            <li><a href="#">Cool stuff</a></li>
+            <li><a href="#">Random feature</a></li>
+            <li><a href="#">Team feature</a></li>
         </ul>        
       </div>
       <div class="col-6 col-md">
@@ -15,28 +15,28 @@
             <a href=""><i class="fab fa-instagram fa-3x footer-icon"></i></a>
             <a href=""><i class="fab fa-google-plus-square fa-3x footer-icon"></i></a><br><br><br>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Team feature</a></li>
-          <li><a class="text-muted" href="#">Stuff for developers</a></li>
-          <li><a class="text-muted" href="#">Another one</a></li>
-          <li><a class="text-muted" href="#">Last time</a></li>
+          <li><a href="#">Team feature</a></li>
+          <li><a href="#">Stuff for developers</a></li>
+          <li><a href="#">Another one</a></li>
+          <li><a href="#">Last time</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
         <h5>COMPANY</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Resource</a></li>
-          <li><a class="text-muted" href="#">Resource name</a></li>
-          <li><a class="text-muted" href="#">Another resource</a></li>
-          <li><a class="text-muted" href="#">Final resource</a></li>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Locations</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Terms</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
         <h5>HELP</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Team</a></li>
-          <li><a class="text-muted" href="#">Locations</a></li>
-          <li><a class="text-muted" href="#">Privacy</a></li>
-          <li><a class="text-muted" href="#">Terms</a></li>
+          <li><a href="#">Cool Stuff 1</a></li>
+          <li><a href="#">Cool Stuff 2</a></li>
+          <li><a href="#">Cool Stuff 3</a></li>
+          <li><a href="#">Cool Stuff 4</a></li>
         </ul>
       </div>
     </div>
