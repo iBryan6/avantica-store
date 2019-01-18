f182c45e720cb1e5351274ded223c885f6fc718c
Bryan Argandoña <bryan.argandona@avantica.net>
2019-01-18 10:43:55 -0400

    Footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top footer container-fluid">
     <div class="row">
       <div class="col-6 col-md">
-        <h6><a href="tel:+59176953543" class="footer-phone"><i class="fas fa-phone"></i> +591 76953543</a></h6><br>
-        <h5><a href="mailto:avantica_store@gmail.com"><i class="fas fa-envelope"></i> avantica_store@gmail.com</a></h5><br>
+            <h6><a href="tel:+59176953543" class="footer-phone"><i class="fas fa-phone"></i> +591 76953543</a></h6><br>
+            <h6><a href="mailto:avantica_store@gmail.com" class="footer-email"><i class="fas fa-envelope"></i> avantica_store@gmail.com</a></h6><br>
         <ul class="list-unstyled text-small">
             <li><a href="#">Cool stuff</a></li>
             <li><a href="#">Random feature</a></li>
@@ -10,10 +10,10 @@
         </ul>        
       </div>
       <div class="col-6 col-md">
-            <a href=""><i class="fab fa-facebook-square fa-3x"></i></a>
-            <a href=""><i class="fab fa-twitter-square fa-3x footer-icon"></i></a>
-            <a href=""><i class="fab fa-instagram fa-3x footer-icon"></i></a>
-            <a href=""><i class="fab fa-google-plus-square fa-3x footer-icon"></i></a><br><br><br>
+            <a href=""><i class="fab fa-facebook-square footer-icon"></i></a>
+            <a href=""><i class="fab fa-twitter-square footer-icon"></i></a>
+            <a href=""><i class="fab fa-instagram footer-icon"></i></a>
+            <a href=""><i class="fab fa-google-plus-square footer-icon"></i></a><br><br><br>
         <ul class="list-unstyled text-small">
           <li><a href="#">Team feature</a></li>
           <li><a href="#">Stuff for developers</a></li>
@@ -28,6 +28,8 @@
             <li><a href="#">Locations</a></li>
             <li><a href="#">Privacy</a></li>
             <li><a href="#">Terms</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Terms</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
@@ -37,6 +39,8 @@
           <li><a href="#">Cool Stuff 2</a></li>
           <li><a href="#">Cool Stuff 3</a></li>
           <li><a href="#">Cool Stuff 4</a></li>
+          <li><a href="#">Cool Stuff 5</a></li>
+          <li><a href="#">Cool Stuff 6</a></li>
         </ul>
       </div>
     </div>
