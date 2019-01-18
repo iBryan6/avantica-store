8aecaf9ab4a53a6abd1185e927f993d33da7ab5e
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-18 11:19:19 -0400

    added styles for the navbar hover

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -82,7 +82,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                           
+                            </ol>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
