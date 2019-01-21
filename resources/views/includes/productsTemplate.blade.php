080959b3d931789f58d4316d59e207083d0f3b6f
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-21 16:25:57 -0400

    Added product page

--- resources/views/includes/productsTemplate.blade.php
+++ resources/views/includes/productsTemplate.blade.php
@@ -1,9 +1,16 @@
 <div class="card" style="width: 18rem;">
+    
     <div class="card-header">
-        <h5 class="card-title" style="float:left;">Milk Pil</h5>
-        <a href="#"><i class="fas fa-cart-plus fa-2x" style="float:right;"></i></a>
+        <a href="/product">
+            <h5 class="card-title" style="float:left;">Milk Pil</h5>
+        </a>
+        <a href="#">
+            <i class="fas fa-cart-plus fa-2x" style="float:right;"></i>
+        </a>
     </div>
-    <img class="card-img-top" src="images/lechepil.png" alt="Card image placeholder">
+    <a href="/product">
+        <img class="card-img-top" src="images/lechepil.png" alt="Card image placeholder">
+    </a>
     <div class="card-body">
         <h5 class="card-title">Milk 3Lt.</h5>
         <span class="price-before text-muted">$ 5</span> <span class="price-now">$ 2.99</span>
