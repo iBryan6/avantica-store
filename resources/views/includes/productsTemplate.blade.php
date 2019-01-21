ee6fac50127d259afdd0f7617bcf7a7c9a75dcfc
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-21 15:31:41 -0400

    changed Pil to Milk Pil

--- resources/views/includes/productsTemplate.blade.php
+++ resources/views/includes/productsTemplate.blade.php
@@ -1,6 +1,6 @@
 <div class="card" style="width: 18rem;">
     <div class="card-header">
-        <h5 class="card-title" style="float:left;">Pil</h5>
+        <h5 class="card-title" style="float:left;">Milk Pil</h5>
         <a href="#"><i class="fas fa-cart-plus fa-2x" style="float:right;"></i></a>
     </div>
     <img class="card-img-top" src="images/lechepil.png" alt="Card image placeholder">
