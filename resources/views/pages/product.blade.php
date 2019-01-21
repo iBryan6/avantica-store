81d70df1bad15d6df542babff589b010f23c8271
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-21 18:11:58 -0400

    fixing the responsive page

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -1,58 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="row ml-4 mr-4 mb-4 mt-2">
-            <div class="col-md mt-3">
-                <div class="container">
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#">Food</a></li>
-                                <li class="breadcrumb-item"><a href="#">Milk</a></li>
-                                <li class="breadcrumb-item"><a href="#">PIL Andina</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Chocolatada</li>
-                            </ol>
-                        </nav>
-                <div class="row ml-1 mt-2">
-                    <div class="col-sm img-hidden">
-                        <div class="row">
-                                <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110" alt="">
+<div class="row ml-4 mr-4 mb-4 mt-2">
+    <div class="col-md mt-3">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Food</a></li>
+                    <li class="breadcrumb-item"><a href="#">Milk</a></li>
+                    <li class="breadcrumb-item"><a href="#">PIL Andina</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Chocolatada</li>
+                </ol>
+            </nav>
+            <div class="row ml-1 mt-2">
+                <div class="col-sm">
+                    <div class="row mt-2">
+                        <div class="col-3 col-sm-6 col-md-12">
+                            <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110"
+                                alt="">
                         </div>
-                        <div class="row mt-2">
-                                <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110" alt="">
+                        <div class="col-3 col-sm-6 col-md-12">
+                            <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110"
+                                alt="">
                         </div>
-                        <div class="row mt-2">
-                                <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110" alt="">
+                        <div class="col-3 col-sm-6 col-md-12">
+                            <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110"
+                                alt="">
                         </div>
-                        <div class="row mt-2">
-                                <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110" alt="">
+                        <div class="col-3 col-sm-6 col-md-12">
+                            <img class="img-fluid img-thumbnail rounded mx-auto d-block" src="https://via.placeholder.com/150x110"
+                                alt="">
                         </div>
-                            
                     </div>
-                    <div class="col-sm">
-                            <img class="img-fluid" src="https://via.placeholder.com/500x500" alt="">
-                    </div>     
+
+
+                </div>
+                <div class="col-sm">
+                    <img class="img-fluid" src="https://via.placeholder.com/500x500" alt="">
                 </div>
             </div>
-            </div>
-            <div class="card col-md mt-4 ml-4 pl-4">
-                    <div class="card-header">
-                            <h2 class="card-title header" style="float:left;">Milk Pil</h2>
-                    </div> 
-                    <div class="card-body mt-4">
-                        <h3 class="price-now">
-                            $ 2.99 
-                            <small class="text-muted ml-4" style="text-decoration: line-through">
-                                $ 5
-                            </small>
-                            <small class="text-muted float-right">
-                                FLAT 24% OFF
-                            </small>
-                        </h3> 
-                    </div>
-                </div>
         </div>
-        <div class="row">
-            <div class="col-sm">
-            </div>
+    </div>
+    <div class="card col-md mt-4 ml-4 pl-4">
+        <div class="card-header">
+            <h2 class="card-title header" style="float:left;">Milk Pil</h2>
         </div>
+        <div class="card-body mt-4">
+            <h3 class="price-now">
+                $ 2.99
+                <small class="text-muted ml-4" style="text-decoration: line-through">
+                    $ 5
+                </small>
+                <small class="text-muted float-right">
+                    FLAT 24% OFF
+                </small>
+            </h3>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm">
+    </div>
+</div>
 @endsection
--- resources/views/pages/productCategories.blade.php
+++ resources/views/pages/productCategories.blade.php
@@ -8,19 +8,19 @@
     @include('includes.sidebar_products_categories')
 
     {{-- Products --}}
-    <div class="col-sm container">
+    <div class="col-12 col-sm-6 col-md-8 container">
         <section class="featured-products mt-4">
             <div class="row card-deck container ml-1">
-                @include('includes.productsTemplate')
-                @include('includes.productsTemplate')
-                @include('includes.productsTemplate')
-                @include('includes.productsTemplate')
+                <div class="col-12 col-sm-6 col-md-3">@include('includes.productsTemplate')</div>
+                <div class="col-12 col-sm-6 col-md-3">@include('includes.productsTemplate')</div>
+                <div class="col-12 col-sm-6 col-md-3">@include('includes.productsTemplate')</div>
+                <div class="col-12 col-sm-6 col-md-3">@include('includes.productsTemplate')</div>
             </div>
             <div class="row card-deck container ml-1 mt-4">
-                @include('includes.productsTemplate')
-                @include('includes.productsTemplate')
-                @include('includes.productsTemplate')
-                @include('includes.productsTemplate')
+                <div class="col-sm-12 col-md-6 col-lg-3">@include('includes.productsTemplate')</div>
+                <div class="col-sm-12 col-md-6 col-lg-3">@include('includes.productsTemplate')</div>
+                <div class="col-sm-12 col-md-6 col-lg-3">@include('includes.productsTemplate')</div>
+                <div class="col-sm-12 col-md-6 col-lg-3">@include('includes.productsTemplate')</div>
             </div>
             {{-- Pagination --}}
             @include('includes.pagination')
