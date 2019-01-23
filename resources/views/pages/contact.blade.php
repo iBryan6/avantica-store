4d6801170692d88335a287fc45dfcc388c0dd3de
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-23 10:41:39 -0400

    added Required for some inputs on the contact us page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -16,7 +16,7 @@
                         <form method="POST" action="">
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Name<label style="color:red">*</label></label>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
                                     name="name" value="{{ old('name') }}" required autofocus>
@@ -29,7 +29,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}<label style="color:red">*</label></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Message</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Message<label style="color:red">*</label></label>
 
                             <div class="col-md-6">
                                 <textarea id="message" type="text" class="form-control" name="message" rows="8" cols="20"></textarea>
