d2edbd5c46912910c76f64fa507213aaf4602666
iBryan6 <Dennis_Bryan@hotmail.com>
2019-01-25 16:16:25 -0400

    user db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('username', 15);
+            $table->string('names', 70);
+            $table->string('last_names', 70);
+            $table->string('address', 255);
             $table->string('email')->unique();
             $table->string('phone_number', 20)->unique();
             $table->string('password');
             $table->tinyInteger('status');
+            $table->integer('iduserType');
+            $table->integer('idcity');
             $table->rememberToken();
             $table->timestamps();
         });
