3fa72755c0ca2bba65361110b6d93e0ba1e1d75d
iBryan6 <Dennis_Bryan@hotmail.com>
2019-01-29 13:22:13 -0400

    user datatables

---
+++ resources/views/includes/dashboardAdmin/users.blade.php
@@ -0,0 +1,17 @@
+<div class="tab-pane fade table-responsive" id="users" role="tabpanel" aria-labelledby="users-tab">
+    <h2>USERS</h2>
+    <table class="table table-striped table-bordered" id="users-table" style="width:100%">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Username</th>
+                <th>Sur Names</th>
+                <th>Last Names</th>
+                <th>Address</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Date Created</th>
+            </tr>
+        </thead>
+    </table>
+</div>
