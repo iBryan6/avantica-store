e0fd36e548343b17af3d88a7061ff3e6acf1d4dd
Rodrigo Renato Perez Ferrufino <r3na_ctm@hotmail.com>
2019-01-30 11:20:55 -0400

    Added city seeder

--- database/seeds/CityTableSeeder.php
+++ database/seeds/CityTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\City;
 use Illuminate\Database\Seeder;
 
 class CityTableSeeder extends Seeder
@@ -11,6 +12,40 @@ class CityTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\City::class, 50)->create();
+        $city = new City();
+        $city->name= 'Cochabamba';
+        $city->save();
+
+        $city = new City();
+        $city->name= 'Santa Cruz';
+        $city->save();
+
+        $city = new City();
+        $city->name= 'La Paz';
+        $city->save();      
+        
+        $city = new City();
+        $city->name= 'Oruro';
+        $city->save();     
+        
+        $city = new City();
+        $city->name= 'Potosi';
+        $city->save();      
+        
+        $city = new City();
+        $city->name= 'Sucre';
+        $city->save();     
+        
+        $city = new City();
+        $city->name= 'Beni';
+        $city->save();    
+        
+        $city = new City();
+        $city->name= 'Pando';
+        $city->save();     
+        
+        $city = new City();
+        $city->name= 'Tarija';
+        $city->save(); 
     }
 }
